7ab88b01acb57a6c6a657e31db0e9e3dbf8c05ea
jonderwaater <jonderwaater@gmail.com>
2016-12-23 17:38:43 +0100

    Update strava-smooth.py

--- strava-smooth.py
+++ strava-smooth.py
@@ -9,7 +9,7 @@ from sys import stderr, stdin
 p = argparse.ArgumentParser(description='''Smoothen last activity on Strava.''')
 p.add_argument('activities', nargs='*', type=argparse.FileType("rb"), default=(stdin,),
                    help="Export gpx, smoothen data, and upload activity")
-p.add_argument('-g', '--graph'    , action='store_true', help="Do not create png file showing original and smoothened data.")
+p.add_argument('-g', '--graph'    , action='store_false', help="Do not create png file showing original and smoothened data.")
 p.add_argument('-u', '--no-upload', action='store_true', help="Do not upload smoothened data to Strava.")
 p.add_argument('-d', '--dry-run'  , action='store_true', help="Do not overwrite original activity when attempting to upload to Strava.")
 
