7d97995b025ff34927aefc73ad2525fc7961886c
jonderwaater <jonderwaater@gmail.com>
2016-12-25 02:55:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ import distutils.core as mod_distutilscore
 from setuptools import setup, find_packages
 
 mod_distutilscore.setup(
-    name='strava-smooth',
+    name='stravasmooth',
     version='0.0.1',
     description='Smoothen latest activity on Strava',
     license='Apache License, Version 2.0',
     author='Jaap Onderwaater',
     author_email='jonderwaater@gmail.com',
-    url='https://github.com/jonderwaater/strava-smooth',
-    packages=['strava_smooth',],
+    url='https://github.com/jonderwaater/stravasmooth',
+    packages=['stravasmooth',],
     install_requires=['stravalib'],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
     ],
-    scripts=['strava-smooth.py']
+    scripts=['stravasmooth.py']
 )
